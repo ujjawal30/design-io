529c8e12ea5cd64061d9786e3445ba45144c2379
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-04-22 21:27:33 +0530

    Memoized activeUsers component

--- components/liveblocks/users/ActiveUsers.tsx
+++ components/liveblocks/users/ActiveUsers.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { useOthers, useSelf } from "@/liveblocks.config";
+import { useMemo } from "react";
 
-import { cn } from "@/lib/utils";
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { cn, generateRandomName } from "@/lib/utils";
+
 import Avatar from "@/components/liveblocks/users/Avatar";
 
 import styles from "./index.module.css";
@@ -13,31 +15,36 @@ const ActiveUsers = () => {
 
   const hasMoreUsers = users.length > 3;
 
-  return (
-    <div className="flex pl-3">
-      {users.slice(0, 3).map(({ connectionId, info }) => (
-        <Avatar key={connectionId} src={info.avatar} name={info.name} />
-      ))}
+  const memoizedActiveUsers = useMemo(
+    () => (
+      <div className="flex pl-3">
+        {users.slice(0, 3).map(({ connectionId, info }) => (
+          <Avatar key={connectionId} src={info.avatar} name={info.name} />
+        ))}
 
-      {hasMoreUsers && (
-        <div
-          className={cn(
-            styles.avatar,
-            "flex items-center justify-center text-lg"
-          )}
-          data-tooltip={`${users.length - 3} more user(s)`}
-        >
-          +{users.length - 3}
-        </div>
-      )}
+        {hasMoreUsers && (
+          <div
+            className={cn(
+              styles.avatar,
+              "flex items-center justify-center text-lg"
+            )}
+            data-tooltip={`${users.length - 3} more user(s)`}
+          >
+            +{users.length - 3}
+          </div>
+        )}
 
-      {currentUser && (
-        <div className="relative ml-8 first:ml-0">
-          <Avatar src={currentUser.info.avatar} name="You" />
-        </div>
-      )}
-    </div>
+        {currentUser && (
+          <div className="relative ml-8 first:ml-0">
+            <Avatar src={currentUser.info.avatar} name="You" />
+          </div>
+        )}
+      </div>
+    ),
+    [users.length]
   );
+
+  return memoizedActiveUsers;
 };
 
 export default ActiveUsers;
