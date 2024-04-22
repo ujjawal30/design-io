c8923be989390b437157f77810f1e7c2ea1b119a
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-04-23 01:24:32 +0530

    Added Left & Right Sidebars

---
+++ components/shared/LeftSidebar.tsx
@@ -0,0 +1,9 @@
+const LeftSidebar = () => {
+  return (
+    <section className="flex flex-col border-t border-primary-grey-200 bg-primary-black text-primary-grey-300 min-w-56 sticky left-0 max-sm:hidden h-full select-none overflow-y-auto pb-20">
+      <h3 className="p-4 text-sm uppercase">Elements</h3>
+    </section>
+  );
+};
+
+export default LeftSidebar;
