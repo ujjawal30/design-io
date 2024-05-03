97bfcee8b66a5cd3f50a64dbe0dde3ae576bf94c
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-05-03 18:25:13 +0530

    Added middleware

---
+++ constants/routes.ts
@@ -0,0 +1,3 @@
+export const signInNonAccessibleRoutes = ["/auth/login", "/auth/register"];
+
+export const signedInAccessibleRoutes = ["/", "/dashboard"];
---
+++ middleware.ts
@@ -0,0 +1,37 @@
+import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
+
+import {
+  signInNonAccessibleRoutes,
+  signedInAccessibleRoutes,
+} from "./constants/routes";
+
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    const {
+      url,
+      nextauth: { token },
+      nextUrl: { pathname },
+    } = req;
+
+    if (token && signInNonAccessibleRoutes.includes(pathname))
+      return NextResponse.redirect(new URL("/", url));
+
+    if (
+      !token &&
+      (signedInAccessibleRoutes.includes(pathname) ||
+        pathname.startsWith("/design"))
+    )
+      return NextResponse.redirect(new URL("/auth/login", url));
+  },
+  {
+    callbacks: {
+      authorized: () => true,
+    },
+  }
+);
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
