9438d40ccf2735655864c6ba29d87e05472f9b97
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-05-06 02:55:09 +0530

    Added error pages and component

--- app/design/[id]/page.tsx
+++ app/design/[id]/page.tsx
@@ -1,14 +1,28 @@
 import dynamic from "next/dynamic";
 
 import LiveRoom from "@/components/liveblocks/LiveRoom";
+import Design from "../Design";
+import { fetchDesign } from "@/lib/actions/design.actions";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import authOptions from "@/auth.config";
 
 interface DesignPageProps {
   params: { id: string };
 }
 
-const Design = dynamic(() => import("../Design"), { ssr: false });
+const DesignPage = async ({ params: { id } }: DesignPageProps) => {
+  const session = await getServerSession(authOptions);
 
-const DesignPage = ({ params: { id } }: DesignPageProps) => {
+  const design = await fetchDesign({ designId: id, populate: true });
+
+  if (!design.status || !design.data) throw new Error(design.message);
+
+  const { creator, collaborators } = design.data;
+  // @ts-ignore
+  const canAccess = creator._id === session?.user.id || collaborators.some((collaborator) => collaborator._id === session?.user.id);
+  if (!canAccess) redirect("/");
+
   return (
     <LiveRoom id={id}>
       <Design />
