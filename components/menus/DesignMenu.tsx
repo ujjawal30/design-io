6c6928628b69eb5b277f935262a4faaa0139fc1d
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2024-05-12 01:31:17 +0530

    Modified design menu

--- components/menus/DesignMenu.tsx
+++ components/menus/DesignMenu.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { EditIcon, InfoIcon, SquareArrowOutUpRightIcon, TrashIcon } from "lucide-react";
+import { EditIcon, HomeIcon, InfoIcon, PlusCircleIcon, SquareArrowOutUpRightIcon, TrashIcon } from "lucide-react";
 
 import { editMetadataModal } from "@/hooks/useModal";
 import {
@@ -11,6 +11,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useRouter } from "next/navigation";
 
 interface DesignMenuProps {
   children: React.ReactElement;
@@ -19,28 +20,38 @@ interface DesignMenuProps {
 }
 
 const DesignMenu = ({ children, design, isCreator }: DesignMenuProps) => {
+  const { push } = useRouter();
   const { onOpen } = editMetadataModal();
 
+  const handleHomeClick = () => push("/dashboard");
+  const handleNewClick = () => onOpen({} as DesignProps);
   const handleEditClick = () => onOpen(design);
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 bg-primary-black border-primary-grey-100 text-white rounded-xl">
-        <DropdownMenuGroup>
-          <DropdownMenuItem className="gap-4" onClick={handleEditClick} disabled={!isCreator}>
-            <EditIcon size={20} />
-            <span>Edit Metadata</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem className="gap-4">
-            <InfoIcon size={20} />
-            <span>Details</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem className="gap-4">
-            <SquareArrowOutUpRightIcon size={20} />
-            <span>Export</span>
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
+        <DropdownMenuItem className="gap-4" onClick={handleHomeClick}>
+          <HomeIcon size={20} />
+          <span>Dashboard</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="gap-4" onClick={handleNewClick}>
+          <PlusCircleIcon size={20} />
+          <span>New Design</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem className="gap-4" onClick={handleEditClick} disabled={!isCreator}>
+          <EditIcon size={20} />
+          <span>Edit Metadata</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="gap-4">
+          <InfoIcon size={20} />
+          <span>Details</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="gap-4">
+          <SquareArrowOutUpRightIcon size={20} />
+          <span>Export</span>
+        </DropdownMenuItem>
         <DropdownMenuSeparator />
         <DropdownMenuItem className="gap-4 text-red-500 hover:!text-red-500" disabled={!isCreator}>
           <TrashIcon size={20} />
